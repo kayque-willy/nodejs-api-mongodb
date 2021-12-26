51ab2386a7c697c99e3b48a39c3c05967174d413
opina-ai <kayque-willy@hotmail.com>
2021-12-26 12:04:40 -0300

    Correção da mensagem inicial do server

--- src/server.js
+++ src/server.js
@@ -8,5 +8,5 @@ const app = require("./app");
 var PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`app listen on port: ${PORT}`);
+    console.log(`http://localhost:${PORT}/api/ listen on port: ${PORT}`);
 });
